72efb578901f3148c2e199f8dc83a4ee386909db
Leonid <manu31051996@gmail.com>
2021-07-19 19:06:30 +0300

    Updates

---
+++ precache-manifest.b06ef435269cbda4cc95b67cff510138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e94fb1093b344733d0dfaf5b96262c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "516ca224b17930b00cf3",
+    "url": "./static/css/main.cb09700a.chunk.css"
+  },
+  {
+    "revision": "e7afe659128f626f5681",
+    "url": "./static/js/2.1727d404.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1727d404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516ca224b17930b00cf3",
+    "url": "./static/js/main.284e89dd.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
